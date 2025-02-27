97d3e2cadf2f2caebcee9ee751c998344b98053b
yankaique <yan.contato.dev@gmail.com>
2025-02-27 08:59:35 -0300

    feat: add eror and verify email page

---
+++ src/components/pages/auth/verify-email/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export function VerifyEmailPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Card className="flex flex-col items-center gap-4 p-4">
+        <Image
+          src="/img/email-bro.svg"
+          alt="Man with email"
+          width={300}
+          height={300}
+        />
+        <h1>Enviamos um link de confirmação para o seu email!</h1>
+        <Link href="/auth/signin">
+          <Button>Voltar</Button>
+        </Link>
+      </Card>
+    </div>
+  );
+}

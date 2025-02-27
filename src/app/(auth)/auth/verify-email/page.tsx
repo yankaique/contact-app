d62e6b2d9e6d878ed2ec6033e0a6431a8813761e
yankaique <yan.contato.dev@gmail.com>
2025-02-27 09:04:40 -0300

    fix: names, spaces and location

--- src/app/(auth)/auth/verify-email/page.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-export default function VerifyEmail() {
-  return <h1>Verify Email</h1>;
-}
---
+++ src/app/(pages)/(auth)/auth/error/page.tsx
@@ -0,0 +1,5 @@
+import { ErrorPage } from '@/components/pages/auth/error';
+
+export default function Error() {
+  return <ErrorPage />;
+}
